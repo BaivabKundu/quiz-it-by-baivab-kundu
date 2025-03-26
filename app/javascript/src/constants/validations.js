ebb7f9b9ec72a0093460ccb6e08cbe18223d57c9
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-03-26 11:25:58 +0530

    Created signup page design

--- app/javascript/src/constants/validations.js
+++ app/javascript/src/constants/validations.js
@@ -10,3 +10,23 @@ export const loginSchema = yup.object({
     .required("Password is required")
     .min(8, "Password must be at least 8 characters"),
 });
+
+export const signupSchema = yup.object({
+  username: yup
+    .string()
+    .required("Name is required")
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be less than 50 characters"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("Invalid email format"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters"),
+  password_confirmation: yup
+    .string()
+    .required("Password confirmation is required")
+    .oneOf([yup.ref("password"), null], "Passwords must match"),
+});

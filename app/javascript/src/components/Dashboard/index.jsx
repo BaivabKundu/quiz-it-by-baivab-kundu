ef2abbaa6bb4278f9ef7d51faa9b2ce312394fe6
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-03-28 11:48:49 +0530

    Created quiz table

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-import NeetoHeader from "components/commons/Header";
+import NeetoHeader from "./Header";
 
-const Dashboard = () => <NeetoHeader />;
+import QuizList from "../Quiz";
+
+const Dashboard = () => (
+  <div className="ml-16 flex w-full flex-1 flex-col overflow-hidden">
+    <NeetoHeader />
+    <QuizList />
+  </div>
+);
 
 export default Dashboard;

c24637eb8d744c88dcf8ff88b24fbc61abe7b88d
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-04 16:37:25 +0530

    Created submissions controller and added index action, and also refactored questions and quizzes controller

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -4,30 +4,25 @@ class Api::V1::QuestionsController < ApplicationController
   before_action :load_quiz, only: [:index, :create, :show, :update, :destroy]
   before_action :load_question, only: [:update, :destroy]
 
-  # GET /api/v1/quizzes/:quiz_slug/questions
   def index
     @questions = @current_quiz.questions.where(quiz_id: @current_quiz.id).order(created_at: :desc)
   end
 
-  # GET /api/v1/quizzes/:quiz_slug/questions/:id
   def show
     @question = @current_quiz.questions.find(params[:id])
   end
 
-  # POST /api/v1/quizzes/:quiz_slug/questions
   def create
     question = @current_quiz.questions.new(question_params)
     question.save!
     render_notice(t("successfully_created", entity: "Question"))
   end
 
-  # PATCH/PUT /api/v1/quizzes/:quiz_slug/questions/:id
   def update
     @question.update!(question_params)
     render_notice(t("successfully_updated", entity: "Question"))
   end
 
-  # DELETE /api/v1/quizzes/:quiz_slug/questions/:id
   def destroy
     @question.destroy!
     render_notice(t("successfully_deleted", entity: "Question"))

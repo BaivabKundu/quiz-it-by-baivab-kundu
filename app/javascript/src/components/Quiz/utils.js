7b6451ac40df256315addae51be953f82d2ce1dc
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-07 11:25:34 +0530

    Added all handler functions to separate utilities file

---
+++ app/javascript/src/components/Quiz/utils.js
@@ -0,0 +1,137 @@
+export const handlePublish = (
+  slug,
+  status,
+  updateQuiz,
+  reloadQuizzes,
+  setQuizzes,
+  setError
+) => {
+  updateQuiz(
+    {
+      slug,
+      payload: { status: status === "published" ? "draft" : "published" },
+    },
+    {
+      onSuccess: () => {
+        reloadQuizzes();
+        setQuizzes(prevQuizzes =>
+          prevQuizzes.map(quiz =>
+            quiz.slug === slug
+              ? {
+                  ...quiz,
+                  status: status === "published" ? "Draft" : "Published",
+                }
+              : quiz
+          )
+        );
+      },
+      onError: error => {
+        setError(error.message);
+      },
+    }
+  );
+};
+
+export const handleClone = (slug, cloneQuiz) => {
+  cloneQuiz({
+    slug,
+  });
+};
+
+export const handleDelete = (slug, setQuizToDelete, setShowDeleteAlert) => {
+  setQuizToDelete(slug);
+  setShowDeleteAlert(true);
+};
+
+export const confirmDelete = (
+  quizToDelete,
+  deleteQuiz,
+  setQuizzes,
+  setShowDeleteAlert,
+  setQuizToDelete,
+  setError
+) => {
+  deleteQuiz(quizToDelete, {
+    onSuccess: () => {
+      setQuizzes(prevQuizzes =>
+        prevQuizzes.filter(quiz => quiz.slug !== quizToDelete)
+      );
+      setShowDeleteAlert(false);
+      setQuizToDelete(null);
+    },
+    onError: error => {
+      setError(error.message);
+      setShowDeleteAlert(false);
+      setQuizToDelete(null);
+    },
+  });
+};
+
+export const handleColumnVisibilityChange = (
+  checkedColumns,
+  setVisibleColumnKeys
+) => {
+  setVisibleColumnKeys(checkedColumns.map(col => col.key));
+};
+
+export const handleBulkUpdate = (
+  bulkUpdateQuizzes,
+  selectedRowKeys,
+  updateFields,
+  reloadQuizzes,
+  setSelectedRowKeys
+) => {
+  bulkUpdateQuizzes(
+    { id: selectedRowKeys, update_fields: updateFields },
+    {
+      onSuccess: () => {
+        reloadQuizzes();
+        setSelectedRowKeys([]);
+      },
+    }
+  );
+};
+
+export const handleBulkDelete = (
+  bulkDeleteQuizzes,
+  selectedRowKeys,
+  reloadQuizzes,
+  setSelectedRowKeys,
+  setShowBulkDeleteAlert
+) => {
+  bulkDeleteQuizzes(
+    { id: selectedRowKeys },
+    {
+      onSuccess: () => {
+        reloadQuizzes();
+        setSelectedRowKeys([]);
+        setShowBulkDeleteAlert(false);
+      },
+    }
+  );
+};
+
+export const columns = [
+  {
+    dataIndex: "name",
+    key: "name",
+    title: "Name",
+    isHidable: false,
+  },
+  { dataIndex: "category", key: "category", title: "Category" },
+  {
+    dataIndex: "submissionsCount",
+    key: "submissionsCount",
+    title: "Submissions count",
+  },
+  {
+    dataIndex: "createdAt",
+    key: "createdAt",
+    title: "Created on",
+  },
+  {
+    dataIndex: "status",
+    key: "status",
+    title: "Status",
+  },
+];

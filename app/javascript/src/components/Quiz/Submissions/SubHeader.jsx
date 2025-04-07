e91b4fdddd4883c54dbebd8cffbd5aa53aa35093
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-07 17:10:45 +0530

    Enhanced SubHeader component to support PDF report generation and downloading, including modal for progress indication and error handling

--- app/javascript/src/components/Quiz/Submissions/SubHeader.jsx
+++ app/javascript/src/components/Quiz/Submissions/SubHeader.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Typography } from "@bigbinary/neetoui";
+import { Modal, ProgressBar, Toastr, Typography } from "@bigbinary/neetoui";
+import createConsumer from "channels/consumer";
+import { subscribeToReportDownloadChannel } from "channels/reportDownloadChannel";
+import FileSaver from "file-saver";
+import {
+  useDownloadPdf,
+  useGeneratePdf,
+} from "hooks/reactQuery/useSubmissionsApi";
 import SubHeader from "neetomolecules/SubHeader";
 import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const SubHeaderComponent = ({
   submissionResponse,
@@ -14,47 +22,104 @@ const SubHeaderComponent = ({
 }) => {
   const { t } = useTranslation();
 
+  const [progress, setProgress] = useState(0);
+  const [message, setMessage] = useState("Downloading report");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { slug } = useParams();
+
+  const consumer = createConsumer();
+
+  const { mutate: generatePdf } = useGeneratePdf(slug);
+  const { mutate: downloadPdf } = useDownloadPdf();
+
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleGeneratePdf = () => generatePdf();
+
+  const handleDownloadPdf = () => {
+    downloadPdf(null, {
+      onSuccess: data => {
+        FileSaver.saveAs(data, "quiz_submission_report.pdf");
+        closeModal();
+      },
+      onError: () => {
+        Toastr.error("Failed to download the report");
+        closeModal();
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (progress === 100) {
+      handleDownloadPdf();
+    }
+  }, [progress]);
+
+  const handleDownloadClick = () => {
+    setIsModalOpen(true);
+    subscribeToReportDownloadChannel({
+      consumer,
+      setMessage,
+      setProgress,
+      generatePdf: handleGeneratePdf,
+    });
+  };
+
   return (
-    <SubHeader
-      leftActionBlock={
-        <Typography component="h4" style="h4">
-          {submissionResponse.length} submissions
-        </Typography>
-      }
-      rightActionBlock={
-        <SubHeader.RightBlock
-          downloadButtonProps={{}}
-          columnsButtonProps={{
-            localStorageKey: "my-submission-table-columns",
-            columnData: columns,
-            actionBlock: null,
-            isSearchable: false,
-            buttonProps: {
-              tooltipProps: {
-                content: t("labels.tooltips.columns"),
-                position: "bottom",
-                touch: ["hold", 500],
+    <div>
+      <SubHeader
+        leftActionBlock={
+          <Typography component="h4" style="h4">
+            {submissionResponse.length} submissions
+          </Typography>
+        }
+        rightActionBlock={
+          <SubHeader.RightBlock
+            columnsButtonProps={{
+              localStorageKey: "my-submission-table-columns",
+              columnData: columns,
+              actionBlock: null,
+              isSearchable: false,
+              buttonProps: {
+                tooltipProps: {
+                  content: t("labels.tooltips.columns"),
+                  position: "bottom",
+                  touch: ["hold", 500],
+                },
               },
-            },
-            onChange: checkedColumns =>
-              handleColumnVisibilityChange(checkedColumns),
-            className: "pb-2 px-2",
-          }}
-          filterProps={{
-            isOpen: isFilterPaneOpen,
-            setIsOpen: setIsFilterPaneOpen,
-            columns: filterColumns,
-            buttonProps: {
-              tooltipProps: {
-                content: t("labels.tooltips.filters"),
-                position: "bottom",
-                touch: ["hold", 500],
+              onChange: checkedColumns =>
+                handleColumnVisibilityChange(checkedColumns),
+              className: "pb-2 px-2",
+            }}
+            downloadButtonProps={{
+              onClick: handleDownloadClick,
+            }}
+            filterProps={{
+              isOpen: isFilterPaneOpen,
+              setIsOpen: setIsFilterPaneOpen,
+              columns: filterColumns,
+              buttonProps: {
+                tooltipProps: {
+                  content: t("labels.tooltips.filters"),
+                  position: "bottom",
+                  touch: ["hold", 500],
+                },
               },
-            },
-          }}
-        />
-      }
-    />
+            }}
+          />
+        }
+      />
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <div className="space-y-2 p-6">
+          <p className="text-xl font-semibold">{message}</p>
+          <ProgressBar
+            progressPercentage={progress}
+            progressValue={`${progress}%`}
+          />
+        </div>
+      </Modal>
+    </div>
   );
 };
 

7edd65f4c13c659654e2273043173486217287cf
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-07 11:24:57 +0530

    Refactored Quiz directory code

---
+++ app/javascript/src/components/Quiz/SubHeader.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+import { Typography, Button } from "@bigbinary/neetoui";
+import SubHeader from "neetomolecules/SubHeader";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { buildUrl } from "utils/url";
+
+import BulkActions from "./BulkActions";
+
+import routes from "../../routes";
+
+const QuizSubHeader = ({
+  meta,
+  setIsFilterPaneOpen,
+  handleColumnVisibilityChange,
+  columns,
+  selectedRowKeys,
+  onBulkUpdate,
+  onBulkDelete,
+  filters,
+  onApplyFilters,
+}) => {
+  const history = useHistory();
+  const { t } = useTranslation();
+  const hasSelected = selectedRowKeys.length > 0;
+  const hasFilters = filters && Object.keys(filters).length > 0;
+
+  return (
+    <div className="flex flex-col">
+      <SubHeader
+        leftActionBlock={
+          <>
+            {!hasSelected ? (
+              <div className="flex items-center space-x-2">
+                <Typography component="h4" style="h4">
+                  {t("messages.noOfQuizzes", { noOfQuizzes: meta.totalCount })}
+                </Typography>
+              </div>
+            ) : (
+              <Typography component="h4" style="h4">
+                {selectedRowKeys.length} quizzes selected of {meta.totalCount}
+              </Typography>
+            )}
+            {hasSelected && (
+              <BulkActions
+                selectedRowKeys={selectedRowKeys}
+                onBulkDelete={onBulkDelete}
+                onBulkUpdate={onBulkUpdate}
+              />
+            )}
+          </>
+        }
+        rightActionBlock={
+          <SubHeader.RightBlock
+            columnsButtonProps={{
+              localStorageKey: "my-quiz-table-columns",
+              columnData: columns,
+              actionBlock: null,
+              isSearchable: false,
+              buttonProps: {
+                tooltipProps: {
+                  content: t("labels.tooltips.columns"),
+                  position: "bottom",
+                  touch: ["hold", 500],
+                },
+              },
+              onChange: handleColumnVisibilityChange,
+              className: "pb-2 px-2",
+            }}
+            filterProps={{
+              buttonProps: {
+                tooltipProps: {
+                  content: t("labels.tooltips.filters"),
+                  position: "bottom",
+                  touch: ["hold", 500],
+                },
+                onClick: () => setIsFilterPaneOpen(true),
+              },
+            }}
+          />
+        }
+      />
+      {hasFilters && (
+        <div className="mb-4 flex items-center">
+          <Typography component="span" style="body2">
+            {(filters.selectedCategories?.length > 0
+              ? `Categories: ${filters.selectedCategories.join(", ")}`
+              : "") +
+              (filters.selectedCategories?.length > 0 &&
+              filters.status &&
+              Object.keys(filters.status).length > 0
+                ? " | "
+                : "") +
+              (filters.status && Object.keys(filters.status).length > 0
+                ? `Status: ${
+                    filters.status.charAt(0).toUpperCase() +
+                    filters.status.slice(1)
+                  }`
+                : "")}
+          </Typography>
+          {(filters.selectedCategories?.length > 0 ||
+            (filters.status && Object.keys(filters.status).length > 0)) && (
+            <Button
+              className="ml-2"
+              label="Clear filters"
+              style="secondary"
+              onClick={() => {
+                history.push(buildUrl(routes.dashboard));
+                onApplyFilters({});
+              }}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QuizSubHeader;
--- app/javascript/src/components/Quiz/index.jsx
+++ app/javascript/src/components/Quiz/index.jsx
@@ -1,38 +1,41 @@
 import React, { useState } from "react";
 
-import { MenuHorizontal } from "@bigbinary/neeto-icons";
-import {
-  Dropdown,
-  Tooltip,
-  Table as NeetoTable,
-  Typography,
-  Tag,
-  Alert,
-} from "@bigbinary/neetoui";
+import { Table as NeetoTable, Typography, Alert } from "@bigbinary/neetoui";
 import PageLoader from "components/commons/PageLoader";
-import { format } from "date-fns";
 import {
   useFetchQuizzes,
   useUpdateQuiz,
   useDeleteQuiz,
   useCloneQuiz,
+  useBulkUpdateQuizzes,
+  useBulkDeleteQuizzes,
 } from "hooks/reactQuery/useQuizzesApi";
 import useQueryParams from "hooks/useQueryParams";
-import SubHeader from "neetomolecules/SubHeader";
 import { mergeLeft, isEmpty } from "ramda";
 import { useTranslation, Trans } from "react-i18next";
-import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useQuizzesStore } from "stores/useQuizzesStore";
 import { buildUrl } from "utils/url";
 
 import { DEFAULT_PAGE_INDEX } from "./constants";
 import FilterPane from "./FilterPane";
+import { getQuizColumns } from "./QuizColumns";
+import QuizSubHeader from "./SubHeader";
+import {
+  handlePublish,
+  handleClone,
+  handleDelete,
+  confirmDelete,
+  handleColumnVisibilityChange,
+  columns,
+  handleBulkUpdate,
+  handleBulkDelete,
+} from "./utils";
 
 import routes from "../../routes";
 
 const QuizList = () => {
   const [quizzes, setQuizzes] = useState([]);
-  const [error, setError] = useState(null);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [isFilterPaneOpen, setIsFilterPaneOpen] = useState(false);
   const [visibleColumnKeys, setVisibleColumnKeys] = useState([
@@ -43,6 +46,7 @@ const QuizList = () => {
   ]);
   const [showDeleteAlert, setShowDeleteAlert] = useState(false);
   const [quizToDelete, setQuizToDelete] = useState(null);
+  const [showBulkDeleteAlert, setShowBulkDeleteAlert] = useState(false);
 
   const { t } = useTranslation();
 
@@ -106,6 +110,8 @@ const QuizList = () => {
   const { mutate: updateQuiz } = useUpdateQuiz();
   const { mutate: cloneQuiz } = useCloneQuiz();
   const { mutate: deleteQuiz } = useDeleteQuiz();
+  const { mutate: bulkUpdateQuizzes } = useBulkUpdateQuizzes();
+  const { mutate: bulkDeleteQuizzes } = useBulkDeleteQuizzes();
 
   const handleApplyFilters = newFilters => {
     setFilters(newFilters);
@@ -119,243 +125,75 @@ const QuizList = () => {
     );
   }
 
-  if (error) return <div>Error: {error}</div>;
-
   const handlePageNavigation = newPage => {
     history.push(
       buildUrl(routes.dashboard, mergeLeft({ page: newPage }, queryParams))
     );
   };
 
-  const handlePublish = (slug, status) => {
-    updateQuiz(
-      {
-        slug,
-        payload: { status: status === "published" ? "draft" : "published" },
-      },
-      {
-        onSuccess: () => {
-          reloadQuizzes();
-          setQuizzes(prevQuizzes =>
-            prevQuizzes.map(quiz =>
-              quiz.slug === slug
-                ? {
-                    ...quiz,
-                    status: status === "published" ? "Draft" : "Published",
-                  }
-                : quiz
-            )
-          );
-        },
-        onError: error => {
-          setError(error.message);
-        },
-      }
+  const handlePublishWrapper = (slug, status) =>
+    handlePublish(slug, status, updateQuiz, reloadQuizzes, setQuizzes);
+
+  const handleCloneWrapper = slug => handleClone(slug, cloneQuiz);
+
+  const handleDeleteWrapper = slug =>
+    handleDelete(slug, setQuizToDelete, setShowDeleteAlert);
+
+  const confirmDeleteWrapper = () =>
+    confirmDelete(
+      quizToDelete,
+      deleteQuiz,
+      setQuizzes,
+      setShowDeleteAlert,
+      setQuizToDelete
     );
-  };
 
-  const handleClone = slug => {
-    cloneQuiz({
-      slug,
-    });
-  };
+  const handleColumnVisibilityChangeWrapper = checkedColumns =>
+    handleColumnVisibilityChange(checkedColumns, setVisibleColumnKeys);
 
-  const handleDelete = slug => {
-    setQuizToDelete(slug);
-    setShowDeleteAlert(true);
-  };
-
-  const confirmDelete = () => {
-    deleteQuiz(quizToDelete, {
-      onSuccess: () => {
-        setQuizzes(prevQuizzes =>
-          prevQuizzes.filter(quiz => quiz.slug !== quizToDelete)
-        );
-        setShowDeleteAlert(false);
-        setQuizToDelete(null);
-      },
-      onError: error => {
-        setError(error.message);
-        setShowDeleteAlert(false);
-        setQuizToDelete(null);
-      },
-    });
-  };
-
-  const handleColumnVisibilityChange = checkedColumns =>
-    setVisibleColumnKeys(checkedColumns.map(col => col.key));
-
-  const { Menu, MenuItem, Divider } = Dropdown;
-  const { Button: MenuButton } = MenuItem;
-
-  const quizColumns = [
-    {
-      dataIndex: "name",
-      key: "name",
-      title: "Name",
-      render: (text, record) => (
-        <Tooltip content={text} position="top">
-          <Link
-            className="block max-w-xs truncate"
-            to={`/quizzes/${record.slug}/questions`}
-          >
-            {text.length > 30 ? `${text.slice(0, 30)}...` : text}
-          </Link>
-        </Tooltip>
-      ),
-      width: 250,
-    },
-    {
-      dataIndex: "category",
-      key: "category",
-      title: "Category",
-      width: 200,
-    },
-    {
-      dataIndex: "submissionsCount",
-      key: "submissionsCount",
-      title: "Submissions count",
-      width: 150,
-    },
-    {
-      dataIndex: "createdAt",
-      key: "createdAt",
-      title: "Created on",
-      width: 200,
-      render: date => format(new Date(date), "dd MMMM yyyy"),
-    },
-    {
-      dataIndex: "status",
-      key: "status",
-      title: "Status",
-      width: 150,
-      render: status => {
-        status = status === "published" ? "Published" : "Draft";
-
-        return (
-          <Tag
-            label={status}
-            style={status === "Published" ? "info" : "warning"}
-          />
-        );
-      },
-    },
-    {
-      dataIndex: "action",
-      key: "action",
-      title: "Action",
-      render: (_, record) => (
-        <Dropdown
-          buttonStyle="text"
-          icon={MenuHorizontal}
-          position="bottom-end"
-          strategy="fixed"
-        >
-          <Menu>
-            <MenuItem>
-              <MenuButton
-                className="text-black"
-                style="link"
-                onClick={() => handlePublish(record.slug, record.status)}
-              >
-                {record.status === "published" ? "Unpublish" : "Publish"}
-              </MenuButton>
-            </MenuItem>
-            <Divider />
-            <MenuItem>
-              <MenuButton
-                className="text-black"
-                style="link"
-                onClick={() => handleClone(record.slug)}
-              >
-                Clone
-              </MenuButton>
-            </MenuItem>
-            <Divider />
-            <MenuItem>
-              <MenuButton
-                label="Delete"
-                style="danger"
-                type="delete"
-                onClick={() => handleDelete(record.slug)}
-              >
-                Delete
-              </MenuButton>
-            </MenuItem>
-          </Menu>
-        </Dropdown>
-      ),
-      width: 100,
-    },
-  ];
-
-  const columns = [
-    {
-      dataIndex: "name",
-      key: "name",
-      title: "Name",
-      isHidable: false,
-    },
-    { dataIndex: "category", key: "category", title: "Category" },
-    {
-      dataIndex: "submissionsCount",
-      key: "submissionsCount",
-      title: "Submissions count",
-    },
-    {
-      dataIndex: "createdAt",
-      key: "createdAt",
-      title: "Created on",
-    },
-    {
-      dataIndex: "status",
-      key: "status",
-      title: "Status",
-    },
-  ];
+  const quizColumns = getQuizColumns(
+    handlePublishWrapper,
+    handleCloneWrapper,
+    handleDeleteWrapper
+  );
 
   const filteredColumns = quizColumns.filter(
     column => visibleColumnKeys.includes(column.key) || column.key === "action"
   );
 
+  const handleBulkUpdateWrapper = updateFields => {
+    handleBulkUpdate(
+      bulkUpdateQuizzes,
+      selectedRowKeys,
+      updateFields,
+      reloadQuizzes,
+      setSelectedRowKeys
+    );
+  };
+
+  const handleBulkDeleteWrapper = () => {
+    handleBulkDelete(
+      bulkDeleteQuizzes,
+      selectedRowKeys,
+      reloadQuizzes,
+      setSelectedRowKeys,
+      setShowBulkDeleteAlert
+    );
+  };
+
   return (
     <div className="px-4 py-8">
-      <SubHeader
-        leftActionBlock={
-          <Typography component="h4" style="h4">
-            {t("messages.noOfQuizzes", { noOfQuizzes: quizResponse.length })}
-          </Typography>
-        }
-        rightActionBlock={
-          <SubHeader.RightBlock
-            columnsButtonProps={{
-              localStorageKey: "my-quiz-table-columns",
-              columnData: columns,
-              actionBlock: null,
-              isSearchable: false,
-              buttonProps: {
-                tooltipProps: {
-                  content: t("labels.tooltips.columns"),
-                  position: "bottom",
-                  touch: ["hold", 500],
-                },
-              },
-              onChange: checkedColumns =>
-                handleColumnVisibilityChange(checkedColumns),
-              className: "pb-2 px-2",
-            }}
-            filterProps={{
-              buttonProps: {
-                tooltipProps: {
-                  content: t("labels.tooltips.filters"),
-                  position: "bottom",
-                  touch: ["hold", 500],
-                },
-                onClick: () => setIsFilterPaneOpen(true),
-              },
-            }}
-          />
-        }
+      <QuizSubHeader
+        columns={columns}
+        filters={filters}
+        handleColumnVisibilityChange={handleColumnVisibilityChangeWrapper}
+        isFilterPaneOpen={isFilterPaneOpen}
+        meta={meta}
+        selectedRowKeys={selectedRowKeys}
+        setIsFilterPaneOpen={setIsFilterPaneOpen}
+        onApplyFilters={handleApplyFilters}
+        onBulkDelete={() => setShowBulkDeleteAlert(true)}
+        onBulkUpdate={handleBulkUpdateWrapper}
       />
       {!isQuizzesLoading ? (
         <div className="custom-table">
@@ -411,7 +249,30 @@ const QuizList = () => {
             </Typography>
           }
           onClose={() => setShowDeleteAlert(false)}
-          onSubmit={confirmDelete}
+          onSubmit={confirmDeleteWrapper}
+        />
+      )}
+      {showBulkDeleteAlert && (
+        <Alert
+          cancelButtonLabel={t("messages.alerts.bulkDeleteQuiz.cancelButton")}
+          isOpen={showBulkDeleteAlert}
+          submitButtonLabel={t("messages.alerts.bulkDeleteQuiz.confirmButton")}
+          title={t("messages.alerts.bulkDeleteQuiz.title")}
+          message={
+            <Typography>
+              <Trans
+                i18nKey="messages.alerts.bulkDeleteQuiz.message"
+                components={{
+                  strong: <strong />,
+                }}
+                values={{
+                  count: selectedRowKeys.length,
+                }}
+              />
+            </Typography>
+          }
+          onClose={() => setShowBulkDeleteAlert(false)}
+          onSubmit={handleBulkDeleteWrapper}
         />
       )}
     </div>

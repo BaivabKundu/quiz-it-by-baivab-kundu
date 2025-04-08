fed5d394a5be2149928d6cce75b50e446717364e
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-08 11:01:13 +0530

    Added QuizPolicy class for managing quiz access permissions

---
+++ app/policies/quiz_policy.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class QuizPolicy
+  attr_reader :user, :quiz
+
+  def initialize(user, quiz)
+    @user = user
+    @quiz = quiz
+  end
+
+  def admin_and_creator?
+    user.admin? && quiz.creator_id == user.id
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.includes(:creator)
+        .where(user_is_admin_and_creator_of_quiz)
+    end
+
+    private
+
+      def user_is_admin_and_creator_of_quiz
+        { creator_id: user.id, users: { role: :admin } }
+      end
+  end
+end

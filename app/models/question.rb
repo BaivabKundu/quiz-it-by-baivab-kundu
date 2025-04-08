b26d37ccb41ea210a267876a94d0076f6aca8051
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-08 10:43:59 +0530

    Updated Question model to ensure that when questions are created or updated or deleted or cloned, the responsible quiz will also be updated

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,19 @@
 
 class Question < ApplicationRecord
   include DeepCloneable
-  belongs_to :quiz, counter_cache: true
+  belongs_to :quiz, counter_cache: true, touch: true
 
   validates :body, presence: true
   validates :options, presence: true
   validates :answer_id, presence: true
+
+  before_update :set_quiz_to_draft_if_published
+
+  private
+
+    def set_quiz_to_draft_if_published
+      if quiz.published? && (body_changed? || options_changed? || answer_id_changed?)
+        quiz.update!(status: :draft)
+      end
+    end
 end

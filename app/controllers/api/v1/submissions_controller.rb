39205b556f9b6ada3d7adda3c0845199543bc8e0
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-10 17:09:30 +0530

    Added create and update actions for submissions, integrated evaluation of quiz result processing, and ordered submissions by creation date in descending order

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::SubmissionsController < ApplicationController
   def index
     @quiz = Quiz.find_by!(slug: params[:slug])
-    submissions = @quiz.submissions
+    submissions = @quiz.submissions.order(created_at: :desc)
     submissions = SearchSubmissionService.new(submissions, params[:search_key]).process
     submissions = FilterSubmissionService.new(submissions, params[:filters]).process
     pagination_service = PaginationService.new(submissions, params)
@@ -12,4 +12,21 @@ class Api::V1::SubmissionsController < ApplicationController
     @submissions = result[:records]
     @meta = result[:meta]
   end
+
+  def create
+    @submission = EvaluationService.new(params).process!
+  end
+
+  def update
+    @submission = Submission.find(params[:id])
+    @submission.update!(submission_params)
+    @submission = EvaluationService.new(params, @submission).process!
+    render_notice("Submission updated successfully!")
+  end
+
+  private
+
+    def submission_params
+      params.require(:submission).permit(:status, answers: [:question_id, :selected_option_index])
+    end
 end

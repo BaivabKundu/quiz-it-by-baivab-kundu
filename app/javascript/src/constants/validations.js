d6f32322d5c41c846fa1e59163f15e236543be3f
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-10 16:57:17 +0530

    Added registration schema for standard user validation who all can only attempt the quizzes

--- app/javascript/src/constants/validations.js
+++ app/javascript/src/constants/validations.js
@@ -35,6 +35,18 @@ export const signupSchema = yup.object({
     ),
 });
 
+export const registrationSchema = yup.object({
+  username: yup
+    .string()
+    .required(t("auth.validations.nameRequired"))
+    .min(2, t("auth.validations.nameMinLength"))
+    .max(50, t("auth.validations.nameMaxLength")),
+  email: yup
+    .string()
+    .required(t("auth.validations.emailRequired"))
+    .email(t("auth.validations.emailInvalid")),
+});
+
 export const filterSchema = yup.object().shape({
   name: yup.string(),
   selectedCategories: yup.array().of(

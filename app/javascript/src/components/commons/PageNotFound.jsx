f179b42e868fe7c6a449ed138c2bbc48bdbc6377
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-04-12 12:52:26 +0530

    Added PageNotFound component and integrated it into the routing of the Main component, and implemented private routing for QuizAttempt page based on registration status, and admin pages

---
+++ app/javascript/src/components/commons/PageNotFound.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { NoData } from "@bigbinary/neetoui";
+import { t } from "i18next";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import withTitle from "utils/withTitle";
+
+const PageNotFound = () => {
+  const history = useHistory();
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex w-full items-center justify-center">
+      <NoData
+        title={t("messages.pageNotFound")}
+        primaryButtonProps={{
+          label: t("labels.buttons.home"),
+          onClick: () => {
+            localStorage.clear();
+            history.replace("/");
+            window.location.reload();
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default withTitle(PageNotFound, t("title.pageNotFound"));
